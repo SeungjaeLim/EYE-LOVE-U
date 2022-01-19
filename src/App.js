735d1a5e06145317cfc8d807d4e976965c4b0cd0
DDoubleA <ace9804@unist.ac.kr>
2022-01-19 20:09:17 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react'
 
 function App() {
+  useEffect(()=>{
+    const webgazer = window.webgazer
+    webgazer.setGazeListener((data, clock)=>{
+      console.log(data, clock);
+    }).begin()
+  })
+  
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
