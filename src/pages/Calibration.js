26996b7d450285bdcd3607eedceb6a599d4a5501
DDoubleA <ace9804@unist.ac.kr>
2022-01-21 15:45:00 +0900

    no red dotand calibration

--- src/pages/Calibration.js
+++ src/pages/Calibration.js
@@ -1,84 +1,35 @@
 import {useState, useEffect} from 'react'
 import './Calibration.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function Calibration() {
+  const navigate = useNavigate();
   const webgazer = window.webgazer
-  const [cnt1, setcnt1] = useState(0);
-  const [cnt2, setcnt2] = useState(0);
   // const [number,setnumber] = useState(0);
-  const [pictures1, setpictures1] = useState([]);
-  const [pictures2, setpictures2] = useState([]);
 
   useEffect(()=>{
     webgazer.clearData()
-    let pictur1 = document.querySelector(".picture1");
-    let picture1 = pictur1.getBoundingClientRect();
-    console.log('picture1', picture1);
-    setpictures1([picture1.top, picture1.bottom, picture1.left, picture1.right]);
-
-    let pictur2 = document.querySelector(".picture2");
-    let picture2 = pictur2.getBoundingClientRect();
-    setpictures2([picture2.top, picture2.bottom, picture2.left, picture2.right]);
+    webgazer.setGazeListener((data, clock)=>{
+    }).begin()
   }, [])
 
-
-  useEffect(()=>{
-
-    // console.log('cnt1', cnt1);
-    
-    webgazer.setGazeListener((data, clock)=>{
-      if (data == null) {
-        return;
-      }
-      // if(data.x > pictures1[0] && data.x < pictures1[1] && data.y >pictures1[2] && data.y < pictures1[3]){
-      //   setcnt1(cnt1 + 1)
-      //   console.log('cnt1', cnt1);
-      // }
-      // if(data.x > pictures2[0] && data.x < pictures2[1] && data.y >pictures2[2] && data.y < pictures2[3]){
-      //   setcnt2(cnt2 + 1)
-      // }
-
-    }).begin()
-  })
-
-  const onClickstart = (target) => {
-    webgazer.end();
-  }
-
-  const onClickfinish = (target) => {
-    webgazer.end();
+  const onClick = (target) => {
+    webgazer.pause();
+    navigate(`/manmatching`);
   }
 
   return (
     <div>
       <div className='top1'> </div>
       Calibration
-
-      <div onClick={(e) => {
-        e.preventDefault();
-        onClickstart(); 
-      }}>
-        start
-      </div>
-
       <div className='top2'></div>
 
       <div onClick={(e) => {
         e.preventDefault();
-        onClickfinish(); 
+        onClick("calibration"); 
       }}>
-        finish
+        Calibration
       </div>
-
-      <div className ='pictures'>
-      <div >
-          <div className ='picture1'></div>
-      </div>
-      <div>
-          <div className ='picture2'></div>
-      </div>
-      </div>
-
 
     </div>
   );
--- src/pages/ManMatching.js
+++ src/pages/ManMatching.js
@@ -1,5 +1,64 @@
+import {useState, useEffect} from 'react'
+import './Calibration.css'
+
 export default function ManMatching() {
+  const webgazer = window.webgazer
+  let cnt1 =0, cnt2 =0;
+  const [pictures1, setpictures1] = useState([]);
+  const [pictures2, setpictures2] = useState([]);
+
+  useEffect(()=>{
+    //그림1 상하좌우 좌표
+    let pictur1 = document.querySelector(".picture1");
+    let picture1 = pictur1.getBoundingClientRect();
+    setpictures1([picture1.top, picture1.bottom, picture1.left, picture1.right]);
+
+    //그림2 상하좌우 좌표
+    let pictur2 = document.querySelector(".picture2");
+    let picture2 = pictur2.getBoundingClientRect();
+    setpictures2([picture2.top, picture2.bottom, picture2.left, picture2.right]);
+
+    webgazer.showPredictionPoints(false) 
+    webgazer.setGazeListener((data, clock)=>{
+    }).begin()
+    
+  }, [])
+
+  // axios.get('localhost:8080/api/download?user_id=??')
+  useEffect(()=>{
+    console.log('cnt1', cnt1);
+      webgazer.setGazeListener((data, clock)=>{
+        if(data == null)
+          return;
+        if(data.y > pictures1[0] && data.y < pictures1[1] && data.x >pictures1[2] && data.x < pictures1[3]){
+        cnt1 = cnt1 + 1;
+        if(cnt1 > 50){
+          window.alert("You Choosed left")
+        }
+      }
+      if(data.y > pictures2[0] && data.y < pictures2[1] && data.x >pictures2[2] && data.x < pictures2[3]){
+        cnt2 += 1;
+        if( cnt2 > 50){
+          window.alert("You Choosed right")
+        }
+      }
+      }).begin()
+  
+  })
+
   return (
-    <div>ManMatching</div>
+    <div>
+      <div className='top1'> </div>
+      Calibration
+      <div className='top2'></div>
+      <div className ='pictures'>
+      <div >
+          <div className ='picture1'></div>
+      </div>
+      <div>
+          <div className ='picture2'></div>
+      </div>
+      </div>
+    </div>
   );
 }
